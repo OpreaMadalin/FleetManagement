a8e8dd6ec05b3eb29573fb5a689bd8beb66e6152
OpreaMadalin <madalinoprea1992@yahoo.com>
2021-02-03 10:50:40 +0200

    Add User Registration

--- src/main/java/com/company/fleetApp/ApplicationController.java
+++ src/main/java/com/company/fleetApp/ApplicationController.java
@@ -25,4 +25,9 @@ public class ApplicationController {
     public String logout() {
         return "login";
     }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
 }
