7d40efe83d347c1a75ce54cc5b25165c1091bed7
OpreaMadalin <madalinoprea1992@yahoo.com>
2021-02-03 11:51:03 +0200

    Password Encoder

--- src/main/java/com/company/fleetApp/ApplicationSecurityConfig.java
+++ src/main/java/com/company/fleetApp/ApplicationSecurityConfig.java
@@ -9,8 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -37,8 +36,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Autowired
@@ -50,7 +49,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
         provider.setUserDetailsService(userDetailsService);
 
-        provider.setPasswordEncoder(passwordEncoder());
+        provider.setPasswordEncoder(bCryptPasswordEncoder());
 
         return provider;
     }
--- src/main/java/com/company/fleetApp/services/UserService.java
+++ src/main/java/com/company/fleetApp/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.company.fleetApp.models.User;
@@ -11,27 +12,31 @@ import com.company.fleetApp.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+
 	@Autowired
 	private UserRepository userRepository;
 	
-	//Get All Users
+
 	public List<User> findAll(){
 		return userRepository.findAll();
 	}	
 	
-	//Get User By Id
+
 	public Optional<User> findById(int id) {
 		return userRepository.findById(id);
 	}	
 	
-	//Delete User
+
 	public void delete(int id) {
 		userRepository.deleteById(id);
 	}
 	
-	//Update User
+
 	public void save(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 
